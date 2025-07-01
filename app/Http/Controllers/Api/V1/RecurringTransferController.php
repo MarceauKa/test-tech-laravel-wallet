04d63f8d1ffe28e0551091eac160db4968a9fc74
Marceau Casals <marceau@casals.fr>
2025-07-01 16:11:51 +0200

    :construction: Create recurring transfer and prepare the web controller

--- app/Http/Controllers/Api/V1/RecurringTransferController.php
+++ app/Http/Controllers/Api/V1/RecurringTransferController.php
@@ -6,34 +6,22 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Actions\PerformWalletTransfer;
 use App\Http\Requests\Api\V1\SendMoneyRequest;
+use App\Http\Requests\CreateRecurringTransferRequest;
+use App\Models\RecurringTransfer;
 use Illuminate\Http\Response;
 
 class RecurringTransferController
 {
-    public function create(SendMoneyRequest $request, PerformWalletTransfer $performWalletTransfer): Response
+    public function create(CreateRecurringTransferRequest $request): Response
     {
-        $recipient = $request->getRecipient();
-
-        $performWalletTransfer->execute(
-            sender: $request->user(),
-            recipient: $recipient,
-            amount: $request->input('amount'),
-            reason: $request->input('reason'),
-        );
+        //
 
         return response()->noContent(201);
     }
 
-    public function delete(PerformWalletTransfer $performWalletTransfer): Response
+    public function delete(RecurringTransfer $transfer): Response
     {
-        $recipient = $request->getRecipient();
-
-        $performWalletTransfer->execute(
-            sender: $request->user(),
-            recipient: $recipient,
-            amount: $request->input('amount'),
-            reason: $request->input('reason'),
-        );
+        //
 
         return response()->noContent(201);
     }
